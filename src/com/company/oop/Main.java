f417fd7e39b6b73b2c745284c6cc5ff28cb4fdb1
yoanaa11 <maximova03@abv.bg>
2023-10-31 09:34:42 +0200

    Initial commit

--- src/com/company/oop/Main.java
+++ src/com/company/oop/Main.java
@@ -1,7 +1,19 @@
 package com.company.oop;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
+        MyList<String> list = new MyListImpl<>();
+        list.add("Item 1");
+        list.add("Item 2");
+        list.add("Item 3");
+        list.add("Item 4");
+
+        list.removeAt(2);
+        list.removeAt(1);
     }
 }
--- src/com/company/oop/MyListImpl.java
+++ src/com/company/oop/MyListImpl.java
@@ -1,6 +1,5 @@
 package com.company.oop;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Iterator;
 import java.util.Objects;
